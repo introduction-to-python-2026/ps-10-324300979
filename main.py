db9d799339e99dbd4ddf292c8e14962c75185a63
324300979 <yaelarbel2002@gmail.com>
2026-01-07 20:54:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from image_utils import load_image, edge_detection
+from PIL import Image
+from skimage.filters import median
+from skimage.morphology import ball
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.ndimage
+from scipy.signal import convolve2d
 
+image = load_image("galil.png")
+clean_image = median(image, ball(3))
+edge_image = edge_detection(clean_image)
+img = Image.fromarray(edge_image)
+img.save('my_edges.png')
